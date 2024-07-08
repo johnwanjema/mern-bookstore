cec6792efffacc1b5b0c0cb7fb278d5e5d390f69
John Wanjema <jonwanjema@gmail.com>
2024-07-08 16:00:14 -0700

    add mongoDB config

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { PORT } from "./config.js"
+import { PORT, mongoDBURL } from "./config.js"
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -7,6 +8,12 @@ app.get('/',(request,response)=>{
     return response.status(234).send('Welcome')
 })
 
-app.listen(PORT, ()=>{
-    console.log(`App is listening to port: ${PORT}`);
+
+mongoose.connect( mongoDBURL).then(()=>{
+    console.log("App connected to DB")
+    app.listen(PORT, ()=>{
+        console.log(`App is listening to port: ${PORT}`);
+    })
+}).catch((error)=>{
+    console.log(error)
 })
