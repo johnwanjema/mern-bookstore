d880db19fc26dba3ee08d15507305c702ec7a772
John Wanjema <jonwanjema@gmail.com>
2024-07-08 15:34:55 -0700

    add route for /

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ import { PORT } from "./config.js"
 
 const app = express();
 
+app.get('/',(request,response)=>{
+    return response.status(234).send('Welcome')
+})
+
 app.listen(PORT, ()=>{
     console.log(`App is listening to port: ${PORT}`);
 })
