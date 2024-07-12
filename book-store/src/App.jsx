5bae39697de3bcdd06a7f8df6e294f1bcbac407e
John Wanjema <jonwanjema@gmail.com>
2024-07-11 21:54:26 -0700

    add react-router-dom

--- book-store/src/App.jsx
+++ book-store/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import {Route,Routes } from 'react-router-dom'
+
+import createBook from './pages/CreateBook'
+import Home from './pages/Home'
+import DeleteBook from './pages/DeleteBook'
+import EditBook from './pages/EditBook'
+import ShowBook from './pages/ShowBook'
 
 const App = () => {
   return (
-    <div className='bg-red-400'>App</div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/books/create' element={<createBook/>}/>
+      <Route path='/books/details/:id' element={<ShowBook/>}/>
+      <Route path='/books/edit/:id' element={<EditBook/>}/>
+      <Route path='/books/delete/:id' element={<DeleteBook/>}/>
+    </Routes>
   )
 }
 
