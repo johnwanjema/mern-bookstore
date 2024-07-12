56ee69664e3c58200da56e0242f370d323315aaa
John Wanjema <jonwanjema@gmail.com>
2024-07-11 22:03:50 -0700

    fix typo for routes

--- book-store/src/App.jsx
+++ book-store/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Route,Routes } from 'react-router-dom'
 
-import createBook from './pages/CreateBook'
+import CreateBook from './pages/CreateBook'
 import Home from './pages/Home'
 import DeleteBook from './pages/DeleteBook'
 import EditBook from './pages/EditBook'
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
-      <Route path='/books/create' element={<createBook/>}/>
+      <Route path='/books/create' element={<CreateBook/>}/>
       <Route path='/books/details/:id' element={<ShowBook/>}/>
       <Route path='/books/edit/:id' element={<EditBook/>}/>
       <Route path='/books/delete/:id' element={<DeleteBook/>}/>
